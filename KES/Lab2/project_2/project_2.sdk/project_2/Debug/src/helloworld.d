33735bb8f646ea496da55509e9c9cb9c7311d7cb
TorstenSchenk <schenk.1@campus.tu-berlin.de>
2018-11-18 14:32:14 +0100

    KES  update Lab1 und Lab2

---
+++ KES/Lab2/project_2/project_2.sdk/project_2/Debug/src/helloworld.d
@@ -0,0 +1,60 @@
+src/helloworld.o src/helloworld.o: ../src/helloworld.c ../src/platform.h \
+ ../src/platform_config.h \
+ ../../project_2_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../project_2_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../project_2_bsp/ps7_cortexa9_0/include/xgpio.h \
+ ../../project_2_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../project_2_bsp/ps7_cortexa9_0/include/xil_assert.h \
+ ../../project_2_bsp/ps7_cortexa9_0/include/xstatus.h \
+ ../../project_2_bsp/ps7_cortexa9_0/include/xgpio_l.h \
+ ../../project_2_bsp/ps7_cortexa9_0/include/xil_io.h \
+ ../../project_2_bsp/ps7_cortexa9_0/include/xpseudo_asm.h \
+ ../../project_2_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h \
+ ../../project_2_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h \
+ ../../project_2_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../project_2_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../project_2_bsp/ps7_cortexa9_0/include/xscugic.h \
+ ../../project_2_bsp/ps7_cortexa9_0/include/xscugic_hw.h \
+ ../../project_2_bsp/ps7_cortexa9_0/include/xil_exception.h \
+ ../../project_2_bsp/ps7_cortexa9_0/include/xil_exception.h \
+ ../../project_2_bsp/ps7_cortexa9_0/include/xil_printf.h
+
+../src/platform.h:
+
+../src/platform_config.h:
+
+../../project_2_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../project_2_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../project_2_bsp/ps7_cortexa9_0/include/xgpio.h:
+
+../../project_2_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../project_2_bsp/ps7_cortexa9_0/include/xil_assert.h:
+
+../../project_2_bsp/ps7_cortexa9_0/include/xstatus.h:
+
+../../project_2_bsp/ps7_cortexa9_0/include/xgpio_l.h:
+
+../../project_2_bsp/ps7_cortexa9_0/include/xil_io.h:
+
+../../project_2_bsp/ps7_cortexa9_0/include/xpseudo_asm.h:
+
+../../project_2_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h:
+
+../../project_2_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h:
+
+../../project_2_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../project_2_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../project_2_bsp/ps7_cortexa9_0/include/xscugic.h:
+
+../../project_2_bsp/ps7_cortexa9_0/include/xscugic_hw.h:
+
+../../project_2_bsp/ps7_cortexa9_0/include/xil_exception.h:
+
+../../project_2_bsp/ps7_cortexa9_0/include/xil_exception.h:
+
+../../project_2_bsp/ps7_cortexa9_0/include/xil_printf.h:
---
+++ KES/Lab2/project_2/project_2.sdk/project_2/Debug/src/platform.d
@@ -0,0 +1,16 @@
+src/platform.o src/platform.o: ../src/platform.c \
+ ../../project_2_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../project_2_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../project_2_bsp/ps7_cortexa9_0/include/xil_cache.h \
+ ../../project_2_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../src/platform_config.h
+
+../../project_2_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../project_2_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../project_2_bsp/ps7_cortexa9_0/include/xil_cache.h:
+
+../../project_2_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../src/platform_config.h:
