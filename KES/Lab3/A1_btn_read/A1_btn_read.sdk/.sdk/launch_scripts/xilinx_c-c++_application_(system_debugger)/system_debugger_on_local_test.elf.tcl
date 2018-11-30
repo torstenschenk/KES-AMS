b5e25458114288fa703077d10b77a1b1056de603
omid-beuth <omid.rahimian.mashhadi@users.noreply.github.com>
2018-11-30 23:57:17 +0100

    sleep (1)

--- KES/Lab3/A1_btn_read/A1_btn_read.sdk/.sdk/launch_scripts/xilinx_c-c++_application_(system_debugger)/system_debugger_on_local_test.elf.tcl
+++ KES/Lab3/A1_btn_read/A1_btn_read.sdk/.sdk/launch_scripts/xilinx_c-c++_application_(system_debugger)/system_debugger_on_local_test.elf.tcl
@@ -1,7 +1,7 @@
 connect -url tcp:127.0.0.1:3121
-source C:/Users/Misca/Documents/KES-AMS/KES/Lab3/A1_btn_read/A1_btn_read.sdk/design_1_wrapper_hw_platform_0/ps7_init.tcl
+source C:/Users/omid/Documents/GitHub/KES-AMS/KES/Lab3/A1_btn_read/A1_btn_read.sdk/design_1_wrapper_hw_platform_0/ps7_init.tcl
 targets -set -filter {name =~"APU" && jtag_cable_name =~ "Digilent Zed 210248A49949"} -index 0
-loadhw C:/Users/Misca/Documents/KES-AMS/KES/Lab3/A1_btn_read/A1_btn_read.sdk/design_1_wrapper_hw_platform_0/system.hdf
+loadhw C:/Users/omid/Documents/GitHub/KES-AMS/KES/Lab3/A1_btn_read/A1_btn_read.sdk/design_1_wrapper_hw_platform_0/system.hdf
 targets -set -filter {name =~"APU" && jtag_cable_name =~ "Digilent Zed 210248A49949"} -index 0
 stop
 ps7_init
@@ -9,6 +9,6 @@ ps7_post_config
 targets -set -nocase -filter {name =~ "ARM*#0" && jtag_cable_name =~ "Digilent Zed 210248A49949"} -index 0
 rst -processor
 targets -set -nocase -filter {name =~ "ARM*#0" && jtag_cable_name =~ "Digilent Zed 210248A49949"} -index 0
-dow C:/Users/Misca/Documents/KES-AMS/KES/Lab3/A1_btn_read/A1_btn_read.sdk/test/Release/test.elf
+dow C:/Users/omid/Documents/GitHub/KES-AMS/KES/Lab3/A1_btn_read/A1_btn_read.sdk/test/Release/test.elf
 targets -set -nocase -filter {name =~ "ARM*#0" && jtag_cable_name =~ "Digilent Zed 210248A49949"} -index 0
 con
