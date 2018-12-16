3b72521d5e52eb5e50c6eb8e374936169c0c2392
ts <torstenschenk22@yahoo.de>
2018-12-16 13:14:02 +0100

    5667ztttttttttttttttttttttttttttttttttt

--- KES/Lab3/A3_vga-v3/A1_vga_2.sdk/.sdk/launch_scripts/xilinx_c-c++_application_(system_debugger)/system_debugger_on_local_chess.elf.tcl
+++ KES/Lab3/A3_vga-v3/A1_vga_2.sdk/.sdk/launch_scripts/xilinx_c-c++_application_(system_debugger)/system_debugger_on_local_chess.elf.tcl
@@ -1,7 +1,7 @@
 connect -url tcp:127.0.0.1:3121
-source H:/KES-181191/Lab3/A3_vga-v3/A1_vga_2.sdk/design_1_wrapper_hw_platform_0/ps7_init.tcl
+source C:/Users/Misca/Documents/KES-AMS/KES/Lab3/A3_vga-v3/A1_vga_2.sdk/design_1_wrapper_hw_platform_0/ps7_init.tcl
 targets -set -filter {name =~"APU" && jtag_cable_name =~ "Digilent Zed 210248A49949"} -index 0
-loadhw H:/KES-181191/Lab3/A3_vga-v3/A1_vga_2.sdk/design_1_wrapper_hw_platform_0/system.hdf
+loadhw C:/Users/Misca/Documents/KES-AMS/KES/Lab3/A3_vga-v3/A1_vga_2.sdk/design_1_wrapper_hw_platform_0/system.hdf
 targets -set -filter {name =~"APU" && jtag_cable_name =~ "Digilent Zed 210248A49949"} -index 0
 stop
 ps7_init
@@ -9,6 +9,6 @@ ps7_post_config
 targets -set -nocase -filter {name =~ "ARM*#0" && jtag_cable_name =~ "Digilent Zed 210248A49949"} -index 0
 rst -processor
 targets -set -nocase -filter {name =~ "ARM*#0" && jtag_cable_name =~ "Digilent Zed 210248A49949"} -index 0
-dow H:/KES-181191/Lab3/A3_vga-v3/A1_vga_2.sdk/chess/Release/chess.elf
+dow C:/Users/Misca/Documents/KES-AMS/KES/Lab3/A3_vga-v3/A1_vga_2.sdk/chess/Release/chess.elf
 targets -set -nocase -filter {name =~ "ARM*#0" && jtag_cable_name =~ "Digilent Zed 210248A49949"} -index 0
 con
--- KES/Lab3/A3_vga-v3/A1_vga_2.sdk/webtalk/sdk_webtalk.tcl
+++ KES/Lab3/A3_vga-v3/A1_vga_2.sdk/webtalk/sdk_webtalk.tcl
@@ -1,6 +1,6 @@
-webtalk_init -webtalk_dir H:\\KES-181191\\Lab3\\A3_vga-v3\\A1_vga_2.sdk\\webtalk
+webtalk_init -webtalk_dir C:\\Users\\Misca\\Documents\\KES-AMS\\KES\\Lab3\\A3_vga-v3\\A1_vga_2.sdk\\webtalk
 webtalk_register_client -client project
-webtalk_add_data -client project -key date_generated -value "Mo Dez 10 21:06:27 2018" -context "software_version_and_target_device"
+webtalk_add_data -client project -key date_generated -value "Thu Dec 13 18:12:11 2018" -context "software_version_and_target_device"
 webtalk_add_data -client project -key product_version -value "SDK v2016.2" -context "software_version_and_target_device"
 webtalk_add_data -client project -key build_version -value "2016.2" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_platform -value "amd64" -context "software_version_and_target_device"
@@ -12,9 +12,9 @@ webtalk_add_data -client project -key target_family -value "NA" -context "softwa
 webtalk_add_data -client project -key target_device -value "NA" -context "software_version_and_target_device"
 webtalk_add_data -client project -key target_package -value "NA" -context "software_version_and_target_device"
 webtalk_add_data -client project -key target_speed -value "NA" -context "software_version_and_target_device"
-webtalk_add_data -client project -key random_id -value "t2puonlmbp12fcmft48us00hvn" -context "software_version_and_target_device"
-webtalk_add_data -client project -key project_id -value "2016.2_12" -context "software_version_and_target_device"
-webtalk_add_data -client project -key project_iteration -value "12" -context "software_version_and_target_device"
+webtalk_add_data -client project -key random_id -value "kmvvptihqh4g5goj3m0qhrhmc8" -context "software_version_and_target_device"
+webtalk_add_data -client project -key project_id -value "2016.2_31" -context "software_version_and_target_device"
+webtalk_add_data -client project -key project_iteration -value "31" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_name -value "" -context "user_environment"
 webtalk_add_data -client project -key os_release -value "" -context "user_environment"
 webtalk_add_data -client project -key cpu_name -value "" -context "user_environment"
@@ -22,12 +22,12 @@ webtalk_add_data -client project -key cpu_speed -value "" -context "user_environ
 webtalk_add_data -client project -key total_processors -value "" -context "user_environment"
 webtalk_add_data -client project -key system_ram -value "" -context "user_environment"
 webtalk_register_client -client sdk
-webtalk_add_data -client sdk -key uid -value "1544470736058" -context "sdk\\\\hardware/1544470736058"
-webtalk_add_data -client sdk -key isZynq -value "true" -context "sdk\\\\hardware/1544470736058"
-webtalk_add_data -client sdk -key Processors -value "2" -context "sdk\\\\hardware/1544470736058"
-webtalk_add_data -client sdk -key VivadoVersion -value "2016.2" -context "sdk\\\\hardware/1544470736058"
-webtalk_add_data -client sdk -key Arch -value "zynq" -context "sdk\\\\hardware/1544470736058"
-webtalk_add_data -client sdk -key Device -value "7z020" -context "sdk\\\\hardware/1544470736058"
-webtalk_add_data -client sdk -key IsHandoff -value "true" -context "sdk\\\\hardware/1544470736058"
-webtalk_transmit -clientid 1313643304 -regid "" -xml H:\\KES-181191\\Lab3\\A3_vga-v3\\A1_vga_2.sdk\\webtalk\\usage_statistics_ext_sdk.xml -html H:\\KES-181191\\Lab3\\A3_vga-v3\\A1_vga_2.sdk\\webtalk\\usage_statistics_ext_sdk.html -wdm H:\\KES-181191\\Lab3\\A3_vga-v3\\A1_vga_2.sdk\\webtalk\\sdk_webtalk.wdm -intro "<H3>SDK Usage Report</H3><BR>"
+webtalk_add_data -client sdk -key uid -value "1544720947534" -context "sdk\\\\hardware/1544720947534"
+webtalk_add_data -client sdk -key isZynq -value "true" -context "sdk\\\\hardware/1544720947534"
+webtalk_add_data -client sdk -key Processors -value "2" -context "sdk\\\\hardware/1544720947534"
+webtalk_add_data -client sdk -key VivadoVersion -value "2016.2" -context "sdk\\\\hardware/1544720947534"
+webtalk_add_data -client sdk -key Arch -value "zynq" -context "sdk\\\\hardware/1544720947534"
+webtalk_add_data -client sdk -key Device -value "7z020" -context "sdk\\\\hardware/1544720947534"
+webtalk_add_data -client sdk -key IsHandoff -value "true" -context "sdk\\\\hardware/1544720947534"
+webtalk_transmit -clientid 765920522 -regid "" -xml C:\\Users\\Misca\\Documents\\KES-AMS\\KES\\Lab3\\A3_vga-v3\\A1_vga_2.sdk\\webtalk\\usage_statistics_ext_sdk.xml -html C:\\Users\\Misca\\Documents\\KES-AMS\\KES\\Lab3\\A3_vga-v3\\A1_vga_2.sdk\\webtalk\\usage_statistics_ext_sdk.html -wdm C:\\Users\\Misca\\Documents\\KES-AMS\\KES\\Lab3\\A3_vga-v3\\A1_vga_2.sdk\\webtalk\\sdk_webtalk.wdm -intro "<H3>SDK Usage Report</H3><BR>"
 webtalk_terminate
